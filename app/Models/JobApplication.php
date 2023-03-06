00d601cd3f4f718358235c544a06a503c623882a
Yukio <yukio5347@gmail.com>
2023-03-06 20:08:58 +0900

    add JobApplication model

---
+++ app/Models/JobApplication.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobApplication extends Model
+{
+    use HasFactory;
+
+    public const GENDER = [
+        'Male',
+        'Female',
+        'Others',
+    ];
+}
