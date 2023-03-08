0c05b8f56aab2c435cfd5a754d519b4ffd29202f
Yukio <yukio5347@gmail.com>
2023-03-08 18:39:48 +0900

    implement JobPostingController@index

---
+++ app/Http/Controllers/JobPostingController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobPosting;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class JobPostingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Jobs/Index', [
+            'paginator' => JobPosting::active()->orderBy('id', 'desc')->paginate(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JobPosting  $jobPosting
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JobPosting $jobPosting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JobPosting  $jobPosting
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JobPosting $jobPosting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JobPosting  $jobPosting
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JobPosting $jobPosting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JobPosting  $jobPosting
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JobPosting $jobPosting)
+    {
+        //
+    }
+}
--- resources/ts/Pages/Jobs/Index.tsx
+++ resources/ts/Pages/Jobs/Index.tsx
@@ -1,9 +1,46 @@
-import { Link, Head } from '@inertiajs/react';
+import { useState } from 'react';
+import { Link } from '@inertiajs/react';
+import Pagination from '@/Components/Pagination';
+import Paginator from '@/Types/Paginator';
+import JobPosting from '@/Types/JobPosting';
 
-export default function Index() {
-    return (
+const Index = ({ paginator }: { paginator: Paginator }) => {
+  const [currentJob, setCurrentJob] = useState(paginator.data[0]);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>, job: JobPosting): void => {
+    event.preventDefault();
+    setCurrentJob(job);
+  };
+
+  return (
+    <>
+      <span>jobs.index | page {paginator.current_page}</span>
+      {paginator.data.length === 0 ? (
+        <p>No jobs found.</p>
+      ) : (
         <>
-            <span>jobs.index</span>
+          <ul>
+            {paginator.data.map((job: JobPosting, index: number) => (
+              <li key={index} className="my-5">
+                <a href={route('jobs.show', job)} className="underline" onClick={(event) => handleClick(event, job)}>
+                  {job.id}: {job.title}
+                </a>
+                / <Link href={route('jobs.edit', job)}>Edit</Link>/{' '}
+                <Link href={route('jobs.destroy', job)}>Delete</Link>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-10">
+            <p>job detail</p>
+            <p>id: {currentJob.id}</p>
+            <p>title: {currentJob.title}</p>
+            <p>description: {currentJob.description}</p>
+          </div>
         </>
-    );
-}
+      )}
+      <Pagination links={paginator.links} />
+    </>
+  );
+};
+
+export default Index;
