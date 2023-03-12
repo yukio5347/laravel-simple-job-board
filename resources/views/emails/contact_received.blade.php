7025c1ead106ba05ea8da910d2072ead7488dff6
Yukio <yukio5347@gmail.com>
2023-03-12 10:28:02 +0900

    add ContactReceived mail

---
+++ resources/views/emails/contact_received.blade.php
@@ -0,0 +1,12 @@
+{{ __('Received a new inquiry') }}
+
+- {{ __('Name') }}: {!! $contact->name !!}
+- {{ __('Email') }}: {!! $contact->email !!}
+- IP Address: {!! $contact->ip_address !!}
+- User Agent: {!! $contact->user_agent !!}
+- {{ __('Message') }}
+{!! $contact->message !!}
+
+───────────────────────────
+{{ config('app.name') }}
+{{ config('app.url') }}
