8a33b1a19bc4aea7e50235cca71e09e5c8fa6fff
Yukio <yukio5347@gmail.com>
2023-03-14 13:14:15 +0900

    add container class to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,20 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+        './app/Models/*.php',
         './resources/views/**/*.blade.php',
         './resources/ts/**/*.tsx',
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
             },
         },
     },
