3f2253354b0a1cee1a90b207bb690a3d3578b244
Yukio <yukio5347@gmail.com>
2023-03-15 13:56:27 +0900

    implement jobs.apply view

--- resources/ts/Pages/Jobs/Apply.tsx
+++ resources/ts/Pages/Jobs/Apply.tsx
@@ -1,4 +1,5 @@
 import { Head, useForm } from '@inertiajs/react';
+import Alert from '@/Components/Alert';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
@@ -7,8 +8,16 @@ import TextInput from '@/Components/TextInput';
 import Select from '@/Components/Select';
 import JobPosting from '@/Types/JobPosting';
 
-const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPosting: JobPosting }) => {
-  const { data, setData, post, processing, errors, reset } = useForm({
+const Edit = ({
+  jobPosting,
+  title,
+  genders,
+}: {
+  jobPosting: JobPosting;
+  title: string;
+  genders: Record<string, string>;
+}) => {
+  const { data, setData, post, processing, errors } = useForm({
     name: '',
     email: '',
     telephone: '',
@@ -32,14 +41,21 @@ const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPos
 
   return (
     <>
-      <Head title="Apply for the job" />
-      <p>
-        You are about to apply for &quot;{jobPosting.title}&quot; by &quot;{jobPosting.company_name}&quot;
-      </p>
-
+      <Head>
+        <title>{title}</title>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <Alert>
+        <p>
+          {__('You are applying for ":title" by :company', {
+            title: jobPosting.title,
+            company: jobPosting.company_name,
+          })}
+        </p>
+      </Alert>
       <form onSubmit={submit}>
         <div className="mt-4">
-          <InputLabel htmlFor="name" value="Name" />
+          <InputLabel htmlFor="name" value={__('Your Name')} isRequired={true} />
           <TextInput
             id="name"
             name="name"
@@ -47,28 +63,30 @@ const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPos
             className="mt-1 block w-full"
             autoComplete="name"
             onChange={handleOnChange}
+            maxLength="255"
             required
           />
           <InputError message={errors.name} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="email" value="Email" />
+          <InputLabel htmlFor="email" value={__('Email Address')} isRequired={true} />
           <TextInput
             id="email"
             type="email"
             name="email"
             value={data.email}
             className="mt-1 block w-full"
-            autoComplete="name"
+            autoComplete="email"
             onChange={handleOnChange}
+            maxLength="255"
             required
           />
           <InputError message={errors.email} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="telephone" value="telephone" />
+          <InputLabel htmlFor="telephone" value={__('Telephone')} />
           <TextInput
             id="telephone"
             name="telephone"
@@ -76,12 +94,13 @@ const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPos
             className="mt-1 block w-full"
             autoComplete="telephone"
             onChange={handleOnChange}
+            maxLength="255"
           />
           <InputError message={errors.telephone} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="address" value="address" />
+          <InputLabel htmlFor="address" value={__('Address')} />
           <TextInput
             id="address"
             name="address"
@@ -89,12 +108,13 @@ const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPos
             className="mt-1 block w-full"
             autoComplete="address"
             onChange={handleOnChange}
+            maxLength="255"
           />
           <InputError message={errors.address} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="birthday" value="birthday" />
+          <InputLabel htmlFor="birthday" value={__('Birthday')} />
           <TextInput
             id="birthday"
             name="birthday"
@@ -102,13 +122,13 @@ const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPos
             className="mt-1 block w-full"
             type="date"
             onChange={handleOnChange}
-            required
+            max={dateToString(new Date())}
           />
           <InputError message={errors.birthday} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="gender" value="gender" />
+          <InputLabel htmlFor="gender" value={__('Gender')} />
           <Select
             id="gender"
             name="gender"
@@ -116,63 +136,66 @@ const Edit = ({ genders, jobPosting }: { genders: Record<string, string>; jobPos
             value={data.gender}
             className="mt-1 block w-full"
             onChange={handleOnChange}
-            required
+            maxLength="255"
           />
           <InputError message={errors.gender} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="summary" value="summary" />
+          <InputLabel htmlFor="summary" value={__('Summary')} isRequired={true} />
           <Textarea
             id="summary"
             name="summary"
             value={data.summary}
             className="mt-1 block w-full"
             onChange={handleOnChange}
+            maxLength="20000"
             required
           />
           <InputError message={errors.summary} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="education" value="education" />
+          <InputLabel htmlFor="education" value={__('Education')} />
           <Textarea
             id="education"
             name="education"
             value={data.education}
             className="mt-1 block w-full"
             onChange={handleOnChange}
-            required
+            maxLength="20000"
           />
           <InputError message={errors.education} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="work_history" value="work_history" />
+          <InputLabel htmlFor="work_history" value={__('Work History')} />
           <Textarea
             id="work_history"
             name="work_history"
             value={data.work_history}
             className="mt-1 block w-full"
             onChange={handleOnChange}
-            required
+            maxLength="20000"
           />
           <InputError message={errors.work_history} className="mt-2" />
         </div>
 
         <div className="mt-4">
-          <InputLabel htmlFor="certificates" value="certificates" />
+          <InputLabel htmlFor="certificates" value={__('Skills and Certificates')} />
           <Textarea
             id="certificates"
             name="certificates"
             value={data.certificates}
             className="mt-1 block w-full"
             onChange={handleOnChange}
-            required
+            maxLength="20000"
           />
           <InputError message={errors.certificates} className="mt-2" />
         </div>
-        <PrimaryButton disabled={processing}>Post</PrimaryButton>
+        <PrimaryButton disabled={processing} className="mt-6">
+          {__('Apply')}
+        </PrimaryButton>
       </form>
     </>
   );
